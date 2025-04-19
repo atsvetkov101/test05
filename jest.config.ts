3b117eb2a6b163858f692d810c8ed45d9890855c
tsvetkovAv <tsvetkovAv@sber-service.ru>
2025-04-19 23:36:28 +0300

    jest.config.ts

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  "moduleFileExtensions": [
+    "js",
+    "json",
+    "ts"
+  ],
+  "rootDir": "jest-test",
+  "testRegex": ".*\\.spec\\.ts$",
+  "transform": {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  "collectCoverageFrom": [
+    "**/*.(t|j)s"
+  ],
+  "coverageDirectory": "../coverage",
+  "testEnvironment": "node"
+};
+
+export default config;
